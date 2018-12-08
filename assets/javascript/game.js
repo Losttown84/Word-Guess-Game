f6e3e56d737bbb97431fc6e76a44b8729e142f57
Mike Plass <43932200+Losttown84@users.noreply.github.com>
2018-12-07 20:46:54 -0600

    added all variables for work

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,9 @@
 var computerChoices =["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
+var wins = 0;
+var losses = 0;
+var guesses = 9;
+
+var winsText = document.getElementById("wins-text");
+var lossesText = document.getElementById("losses-text");
+var guessesText = document.getElementById("guesses-text");
+var yourLettersText = document.getElementById("yourletters-text")
