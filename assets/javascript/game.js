9571bd175f34f64d7b1e03a43fa3818a8cca1246
Mike Plass <43932200+Losttown84@users.noreply.github.com>
2018-12-09 12:29:22 -0600

    game works witbut doesn't show wins,losses,guesses or letter chosen initially

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,28 +4,42 @@ var losses = 0;
 var guesses = 9;
 var guessChoices = [];
 
-//var winsText = document.getElementById("wins-text");
-//var lossesText = document.getElementById("losses-text");
-//var guessesText = document.getElementById("guesses-text");
-//var yourLettersText = document.getElementById("yourletters-text")
+//placeholder for wins, losses, guess and letter guesses
+"<p> Wins: " + wins + "</p>" +
+"<p> Losses:" + losses + "</p>" +
+"<p> Guess Left:" + guesses + "</p>" +
+"<p> Your Guesses:" + guessChoices.join(",") + "</p>";
 
+//on keyup is when the letter the user has chosen will show up under guess Choices
 document.onkeyup = function(event){
 
+    //user guess for letters and telling computer what options are available for letters
     var userGuess = event.key;
     var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
     var options = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
 
-
+    //if user guess equals to computer guess, wins go up by 1
     if (options.indexOf(userGuess) > -1){
         if (userGuess === computerGuess){
             wins++;
             guesses = 9;
             guessChoices = [];
         }
+    //duplicate letters will not affect guesses left    
+        //else{
+          //  if (guessChoices.includes(userGuess){
+           // })
+            //else {
+              //  userGuess--;
+                //guessChoices.push(userGuess);
+            //}
+        //}
+    //if user guess does not equal computer guess, guess left goes down by 1    
     if (userGuess != computerGuess) {
         guesses --;
         guessChoices.push(userGuess);
-    }    
+    }
+    //if user guesses has gotten to 9, losses goes up by 1    
     if (guesses === 0){
         guesses = 9;
         losses ++;
@@ -33,14 +47,13 @@ document.onkeyup = function(event){
     }
 
     }
-
+//variable that will change as the logic 
     var html =
-    "<h1> The Psychic Game </h1>" +
-    "<p> Guess what letter I'm thinking of</p>" +
     "<p> Wins: " + wins + "</p>" +
     "<p> Losses:" + losses + "</p>" +
     "<p> Guess Left:" + guesses + "</p>" +
     "<p> Your Guesses:" + guessChoices.join(",") + "</p>";
 
-    document.querySelector("wins-text", "losses-text", "guesses-text", "yourletters-text" ).innerHTML = html;
+    //document.querySelector("wins-text", "losses-text", "guesses-text", "yourletters-text" ).innerHTML = html
+    document.querySelector("#wins-text", "#losses-text", "#guesses-text", "#yourletters-text").innerHTML = html;
 };
